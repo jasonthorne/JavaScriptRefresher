d6a6a3aa9e538397e5b72ebbe849747f6bbdbc2e
jasonthorne <jasonthorne@live.ie>
2019-04-01 09:27:52 +0100

    Variables and Strings

--- javascript/learnToCode.js
+++ javascript/learnToCode.js
@@ -1,14 +1,22 @@
- 
+ //Ctrl-alt j in chrome for dev tools +++
             
-var name = "Jay";
-var age= 35;
+//var name = "Jay";
+//var age= 35;
+//var aFloat = 35.5;
 
-var msg = "Yo, my name is " + name + " and I am " + age + " dawg!";
 
-//alert(msg);
+//var msg = "Yo, my name is " + name + " and I am " + age + " dawg!";
 
-//Ctrl-alt j in chrome for dev tools +++
 
-console.log(msg);
+var firstName = "John";
+var lastName = "Smith";
+var dataOfBirth = "10-09-82";
+var age = "23";
+var profileImageUrl = "http://myPic.com/pic.jpg";
+
+//concatenation. ('age' is auto converted into string):
+var loginWelcomeMessage = "Welcome " + firstName + ". Happy " + age + "rd birthday!";
+
+console.log(loginWelcomeMessage);
             
        
