b0df0bc60589a4af380a533160f97edc38d0567a
jasonthorne <jasonthorne@live.ie>
2019-04-01 09:10:46 +0100

    Comments and how to link Scripts

---
+++ javascript/learnToCode.js
@@ -0,0 +1,14 @@
+ 
+            
+var name = "Jay";
+var age= 35;
+
+var msg = "Yo, my name is " + name + " and I am " + age + " dawg!";
+
+//alert(msg);
+
+//Ctrl-alt j in chrome for dev tools +++
+
+console.log(msg);
+            
+       
