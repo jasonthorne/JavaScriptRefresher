05e5486edb953e7d0d01bd1c89a4d3489276c370
jasonthorne <jasonthorne@live.ie>
2019-04-03 10:39:34 +0100

    Percentage Calculator - Grabbing Elements with JavaScript

---
+++ PercentageCalculator/calc.js
@@ -0,0 +1,7 @@
+//variables that store a html element:
+var numField1 = document.getElementById('numField1');
+var numField2 = document.getElementById('numField2');
+var resultField = document.getElementById('resultField');
+
+numField1.value = "test"; //'value' for input fields
+resultField.innerText = "test"; //'innerText' for text
